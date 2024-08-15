d993f2b060a465c1d3b258259253a0f460eb3628
Anthony <141673840+AnthonySarmiento1@users.noreply.github.com>
2024-08-15 12:13:16 -0700

    Update BulkUsersCreator.ps1

--- PowerShell Scripts/BulkUsersCreator.ps1
+++ PowerShell Scripts/BulkUsersCreator.ps1
@@ -5,9 +5,16 @@ $password = ConvertTo-SecureString $PASSWORD_FOR_USERS -AsPlainText -Force
 New-ADOrganizationalUnit -Name _USERS -ProtectedFromAccidentalDeletion $false
 
 foreach ($n in $USER_FIRST_LAST_LIST) {
-    $first = $n.Split(" ")[0].ToLower()
-    $last = $n.Split(" ")[1].ToLower()
-    $username = "$($first.Substring(0,1))$($last)".ToLower()
+    $first = $n.Split(" ")[0]
+    $last = $n.Split(" ")[1]
+
+    # Capitalize the first letter of each name and make the rest lowercase
+    $first = $first.Substring(0,1).ToUpper() + $first.Substring(1).ToLower()
+    $last = $last.Substring(0,1).ToUpper() + $last.Substring(1).ToLower()
+
+    # Concatenate first and last name and convert to lowercase
+    $username = "$($first)$($last)".ToLower()
+
     Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Cyan
     
     New-AdUser -AccountPassword $password `
