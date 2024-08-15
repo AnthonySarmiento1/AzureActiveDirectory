74beec8e9521d85dd0bad8b23235d16a48949327
Anthony <141673840+AnthonySarmiento1@users.noreply.github.com>
2024-08-15 13:32:06 -0700

    Update RandomNamesGen.ps1

--- PowerShell Scripts/RandomNamesGen.ps1
+++ PowerShell Scripts/RandomNamesGen.ps1
@@ -1,33 +1,44 @@
-# Define the number of unique names to generate
-$numberOfNames = 1000
-# Define the output file
-$outputFile = ".\names.txt"
-# Function to generate a random name
-function Generate-RandomName {
+$PASSWORD_FOR_USERS   = "AnthonyPass123$"
+$NUMBER_OF_ACCOUNTS_TO_CREATE = 1000
+
+Function generate-random-name() {
     $consonants = @('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z')
     $vowels = @('a','e','i','o','u')
-    if ($consonants.Count -eq 0 -or $vowels.Count -eq 0) {
-        throw "Consonants or vowels array is empty."
-    }
-
     $nameLength = Get-Random -Minimum 3 -Maximum 9
+    $count = 0
     $name = ""
 
-    for ($i = 0; $i -lt $nameLength; $i++) {
-        if ($i % 2 -eq 0) {
-            $index = Get-Random -Minimum 0 -Maximum ($consonants.Count - 1)
-            $name += $consonants[$index]
-        } else {
-            $index = Get-Random -Minimum 0 -Maximum ($vowels.Count - 1)
-            $name += $vowels[$index]
+    while ($count -lt $nameLength) {
+        if ($($count % 2) -eq 0) {
+            $name += $consonants[$(Get-Random -Minimum 0 -Maximum $($consonants.Count - 1))]
         }
+        else {
+            $name += $vowels[$(Get-Random -Minimum 0 -Maximum $($vowels.Count - 1))]
+        }
+        $count++
     }
-    return $name.Substring(0,1).ToUpper() + $name.Substring(1).ToLower()
+
+    return $name
+
 }
-# Initialize a hashset to store unique names
-$uniqueNames = [System.Collections.Generic.HashSet[string]]::new()
-# Generate unique names
-while ($uniqueNames.Count -lt $numberOfNames) {
-    $firstName = Generate-RandomName
-    $lastName = Generate-RandomName
-    $fullName = "$firstName $lastName"
+
+$count = 1
+while ($count -lt $NUMBER_OF_ACCOUNTS_TO_CREATE) {
+    $fisrtName = generate-random-name
+    $lastName = generate-random-name
+    $username = $fisrtName + '.' + $lastName
+    $password = ConvertTo-SecureString $PASSWORD_FOR_USERS -AsPlainText -Force
+
+    Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Cyan
+
+    New-AdUser -AccountPassword $password `
+               -GivenName $firstName `
+               -Surname $lastName `
+               -DisplayName $username `
+               -Name $username `
+               -EmployeeID $username `
+               -PasswordNeverExpires $true `
+               -Path "ou=_EMPLOYEES,$(([ADSI]`"").distinguishedName)" `
+               -Enabled $true
+    $count++
+}
